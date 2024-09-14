116ee25b80c0ca353d34e21aa675008b8e7d59c8
Leihyn <rexzivyson@gmail.com>
2024-09-14 19:51:25 +0100

    Commit changes before installing OpenZeppelin contracts

--- script/DeployFaruqDAO.s.sol
+++ script/DeployFaruqDAO.s.sol
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../forge-std/src/Script.sol";
+import "forge-std/src/Script.sol";
 import "../src/FaruqDAO.sol";
+
+contract DeployFaruqDAO is Script {
+    function run() external {
+        address nftMarketplaceAddress = vm.envAddress("NFT_MARKETPLACE_ADDRESS");
+        address daoNftAddress = vm.envAddress("DAO_NFT_ADDRESS");
+
+        vm.startBroadcast();
+        new FaruqDAO(nftMarketplaceAddress, daoNftAddress);
+        vm.stopBroadcast();
+    }
+}
